267cd51a43cd21707fc07df5c733f4c4fac7934f
bryanica <bicaro@unicesar.edu.co>
2024-11-16 13:45:34 -0500

    changes

--- index.js
+++ index.js
@@ -42,6 +42,19 @@ app.get('/categories/:categoryId/products/:productId', (req, res) => {
     },
 )});
 
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if(limit && offset){
+    res.json({
+      limit,
+      offset
+    });
+  }
+  else{
+    res.send('No se han enviado parametros');
+  }
+});
+
 app.get('/categories', (req, res) => {
   res.json({
         category: [
