226b8c2f8ef60ec71594a9fb452e2f8375c034b0
bryanica <bicaro@unicesar.edu.co>
2024-11-16 19:05:51 -0500

    add method path and delete

--- routers/productsRouters.js
+++ routers/productsRouters.js
@@ -38,4 +38,22 @@ router.post('/', (req, res) => {
   })
 });
 
+router.patch('/:id', (req, res) => {
+  const {id} = req.params;
+  const body = req.body;
+  res.json({
+    message: 'Product updated',
+    data: body,
+    id,
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  const {id} = req.params;
+  res.json({
+    message: 'Product deleted',
+    id,
+  })
+});
+
 module.exports = router;
